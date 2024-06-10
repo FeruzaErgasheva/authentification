edd0303512a23327d7936677bf215507ea34e9c4
Feruza <ergashevaferuzabonuu@gmail.com>
2024-06-10 21:35:02 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/lesson54_authentication/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lesson54_authentication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:lesson54_authentication/services/auth_http_services.dart';
+import 'package:lesson54_authentication/views/screens/home_screen.dart';
+import 'package:lesson54_authentication/views/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final authHttpServices = AuthHttpServices();
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    authHttpServices.checkAuth().then((value) {
+      setState(() {
+        isLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: isLoggedIn ? const HomeScreen() : const LoginScreen(),
+    );
+  }
+}
